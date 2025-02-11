a2b71661349b67a5d2c9d4c61a58f1226fb4b87b
tommaso <ciolacutommaso@itis-molinari.eu>
2025-02-11 10:26:53 +0100

    FILE DATABSE

---
+++ database.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const mysql = require('mysql2');
+let conf = JSON.parse(fs.readFileSync('conf.json'));
+conf.ssl = {
+    ca: fs.readFileSync(__dirname + '/ca.pem')
+}
+const connection = mysql.createConnection(conf);
+
+
+const executeQuery = (sql) => {
+   return new Promise((resolve, reject) => {      
+         connection.query(sql, function (err, result) {
+            if (err) {
+               console.error(err);
+               reject();     
+            }   
+            console.log('done');
+            resolve(result);         
+      });
+   })
+}
+
+const database = {
+    
+}
+
